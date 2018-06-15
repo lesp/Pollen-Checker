1b00ed686bd0e83a81b088097a034d9ae13eb056
Les <lespounder@gmail.com>
2018-06-15 07:06:52 +0100

    Windows Version
    Basic Windows version, works well, needs icons...working on it

---
+++ Windows-Pollen-Checker.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from pypollen import Pollen
+import time
+from win10toast import ToastNotifier
+toaster = ToastNotifier()
+#Images
+while True:
+    send_url = "http://ipinfo.io/json"
+    r = requests.get(send_url)
+    location = json.loads(r.text)
+    location = location["loc"]
+    location = location.split(",")
+    lat = location[0]
+    lon = location[1]
+    pollen_status = Pollen(lat, lon).pollencount
+    toaster.show_toast("The pollen level at your location is ",pollen_status)
+    time.sleep(15)
